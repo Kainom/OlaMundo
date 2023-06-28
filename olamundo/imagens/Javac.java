abce96a25d499261bf02bad3e80e4e957e68921e
Kainã F. R .E <eleuteriokaina@gmail.com>
2023-06-28 16:34:46 -0300

    Teste

---
+++ olamundo/imagens/Javac.java
@@ -0,0 +1,12 @@
+package olamundo.imagens;
+
+public class Javac {
+
+  public void teste(){
+    System.out.println("Olá Mundo");
+  }
+
+  public static void main(String[] args) {
+     System.out.println("Olá Mundo");
+  }
+}
